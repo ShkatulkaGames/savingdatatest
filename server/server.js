61fdf9da8534aa5964fbd0478fcc6c24d188b5e7
ShkatulkaGames <74930691+ShkatulkaGames@users.noreply.github.com>
2022-09-21 13:01:56 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -31,7 +31,9 @@ app.get("/player-data/:id", async (request, response) => {
     } else {
       const newPlayerDataInstance = new playerModel({
         userID: `${request.params.id}`,
-        coins: 0
+        coins: 0,
+        clanname: "DONTEXISTCLANID./1242950918350238m4",
+        clanOwner:"0",
       })
       
       const newPlayerData = await newPlayerDataInstance.save()
@@ -59,7 +61,7 @@ app.post("/player-data/update-coins/:id", async (request, response) => {
     { $set: { coins: request.body.coins } }
     // We set the coins to the coins we received in the body of the request
   );
-  response.send("Updated Database.");
+  response.send("Updated Clan Coins.");
   // Just a response.
 });
 
